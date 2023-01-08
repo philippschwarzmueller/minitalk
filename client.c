9f7db2f8a91f56dcff51ffbd7ed36910232a810b
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-01-08 15:38:25 +0100

    add function to send text

--- client.c
+++ client.c
@@ -6,14 +6,97 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 10:15:53 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/12/12 16:35:03 by pschwarz         ###   ########.fr       */
+/*   Updated: 2023/01/08 15:37:53 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static char	*ft_strtorevb(char *s);
+
 int	main(int argc, char **argv)
 {
-	kill(atoi(argv[argc - 1]), 30);
+	char	*binarystr;
+	int		i;
+
+	binarystr = ft_strtorevb(argv[argc - 1]);
+	i = 0;
+	ft_printf("%s EOL\n", binarystr);
+	while (binarystr[i] != '\0')
+	{
+		if (binarystr[i] == '1')
+			kill(atoi(argv[argc - 2]), SIGUSR1);
+		else if (binarystr[i] == '0')
+			kill(atoi(argv[argc - 2]), SIGUSR2);
+		else
+			ft_printf("error sending signal");
+		i++;
+		sleep(1);
+	}
+	free(binarystr);
 	return (0);
 }
+
+static char	*ft_strtorevb(char *s)
+{
+	char	*res;
+	int		len;
+	int		i;
+	int		k;
+
+	len = ft_strlen(s);
+	res = ft_calloc(len * 8 + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (s[i] != '\0')
+	{
+		while (k < 8)
+		{
+			if (s[i] & (1 << k))
+				res[(i * 8) + 7 - k] = '1';
+			else
+				res[(i * 8) + 7 - k] = '0';
+			k++;
+		}
+		k = 0;
+		i++;
+	}
+	return (res);
+}
+
+/* static char	*ft_stob(char *s)
+{
+	int		len;
+	int		i;
+	int		j;
+	char	ch;
+	char	*res;
+
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	len = ft_strlen(s);
+	res = malloc(len * 8 + 1);
+	if (!res)
+		return (NULL);
+	res[0] = '\0';
+	while (s[i] != '\0')
+	{
+		ch = s[i];
+		while (j < 8)
+		{
+			if (!(ch & 1))
+				ft_strlcat(res, "0", 1);
+			else
+				ft_strlcat(res, "1", 1);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (res);
+}
+ */
--- server.c
+++ server.c
@@ -6,25 +6,65 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 10:16:03 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/12/12 16:36:19 by pschwarz         ###   ########.fr       */
+/*   Updated: 2023/01/08 15:37:41 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 void	signal_handler(int sig);
+int		ft_bintoc(char *b);
 
 int	main(void)
 {
 	ft_printf("%d\n", getpid());
 	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	while (1)
-		sleep(20);
-	return (1);
+		sleep(1);
+	return (0);
 }
 
 void	signal_handler(int sig)
 {
+	static char	*received_char;
+	static int	bit_count;
+
+	bit_count++;
+	if (!received_char)
+		received_char = "";
 	if (sig == 30)
-		ft_printf("Signal received\n");
+		received_char = ft_strjoin(received_char, "1");
+	else if (sig == 31)
+		received_char = ft_strjoin(received_char, "0");
+	if (bit_count == 8)
+	{
+		ft_printf("%c\n", ft_bintoc(received_char));
+		received_char = "";
+		bit_count = 0;
+	}
+}
+
+int	ft_bintoc(char *b)
+{
+	char	res;
+	int		i;
+
+	res = 0;
+	i = 0;
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0')
+		{
+			ft_printf("%d\n", res);
+		}
+		else if (b[i] == '1')
+		{
+			res = res ^ (1 << (7 - i));
+			ft_printf("%d\n", res);
+		}
+		i++;
+	}
+	ft_printf("--> ");
+	return (res);
 }
